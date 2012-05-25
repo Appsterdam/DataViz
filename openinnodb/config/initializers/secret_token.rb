a7aff620c0e49603fc0974b72aa8bf8215c5938a
pR <box.safe@gmail.com>
2012-05-25 20:14:33 +0200

    first commit,just generated

---
+++ openinnodb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openinnodb::Application.config.secret_token = '492a1f3a8eaad01ca7b59ee7ea08577a21a09450a5e25d132ce15f612c0ff3395d5d7c1d3adcc6139161d6598e70b82ffb0bc0d3c5ce68b295228ccc6394c82a'
