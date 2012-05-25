cc726b0e54f351b854441e09e7074b92662cb414
pR <box.safe@gmail.com>
2012-05-25 19:58:46 +0200

    deleted test innodb project

---
+++ innodb/app/models/mob_app.rb
@@ -0,0 +1,3 @@
+class MobApp
+  # To change this template use File | Settings | File Templates.
+end
---
+++ innodb/app/models/user.rb
@@ -0,0 +1,3 @@
+class User
+  # To change this template use File | Settings | File Templates.
+end
